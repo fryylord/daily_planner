5c6c231efa08155b4c2a0790e84149448794e712
fryylord <rob@mchays.com>
2022-10-12 19:59:02 -0500

    changed some styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,6 @@
       
       if (currentHour == timeDiv) {
         $(this).addClass("present");
-        $(this).children(".description").addClass("white-text");
       } else if (currentHour < timeDiv) {
         $(this).removeClass("present");
         $(this).addClass("future");
